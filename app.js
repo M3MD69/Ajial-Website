df484a54a61a14a77d9850d1ea70a2fef2d2c2d3
مُحَمَّد سَعْدَاوِيْ <shadowsorrow25900@gmail.com>
2023-11-24 11:04:50 +0200

    Simple Edit

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ document.getElementById('downloadLatestVersionBtn').addEventListener('click', do
 
 function downloadLatestAppVersion() {
     const link = document.createElement('a');
-    link.href = 'Ajial.apk';
-    link.download = 'Ajial.apk';
+    link.href = 'Ajial-1.1.apk';
+    link.download = 'Ajial-1.1.apk';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
